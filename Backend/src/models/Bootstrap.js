3c5007775e48e35918f7fcb80966d6c86efa08b6
Yunthecloud <sulei.yin1@kmutt.ac.th>
2025-05-14 23:46:53 +0700

    [update] swarm and bootstrap schema

--- Backend/src/models/Bootstrap.js
+++ Backend/src/models/Bootstrap.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const bootstrapSchema = new mongoose.Schema({
   peerId: { type: String, default: null },
   swarms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Swarm', required: false }],
+  isUsed: { type: Boolean, default: false },
 });
 
 const Auth = mongoose.model('Bootstrap', bootstrapSchema);
--- Backend/src/models/Swarm.js
+++ Backend/src/models/Swarm.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const swarmSchema = new mongoose.Schema({
   swarmkey: { type: String, required: true },
   password: { type: String, required: true },
-  isUsed: { type: Boolean, default: false },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Auth' }],
   bootstrapId: { type: mongoose.Schema.Types.ObjectId, ref: 'Bootstrap',required: false },
 });
